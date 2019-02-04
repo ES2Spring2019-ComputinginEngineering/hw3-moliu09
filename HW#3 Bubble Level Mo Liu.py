b3f2d3140599559c1d3381e0df382d17cebed91c
Mo <mo.liu@tufts.edu>
2019-02-04 16:16:16 -0500

    Update HW#3 Bubble Level Mo Liu.py

--- HW#3 Bubble Level Mo Liu.py
+++ HW#3 Bubble Level Mo Liu.py
@@ -1,6 +1,6 @@
 #Name: Mo Liu
 #Partner: Bo Eaves
-#I worked on this assignmeng with Bo with assistance from Olivia.
+#I worked on this assignmeng with Bo with assistance from Olivia and Brian.
 
 import microbit
 import math #importing the libraries
@@ -8,10 +8,34 @@ import math #importing the libraries
 def measuring_tilt_in_3D(a, b, c):
     return math.atan2(a, math.sqrt(b**2+c**2))
 
+BOAT = microbit.Image("05050:05050:05050:99999:09990")
+
 while True:
     microbit.sleep(100)
     (x, y, z) = microbit.accelerometer.get_values() #this function doesn't take any parameter
-    Ax = measuring_tilt_in_3D(x, y, z)
-    Ay = measuring_tilt_in_3D(y, x, z)
-    print("x = ",math.degrees(Ax))
-    print("y = ",math.degrees(Ay))
+    Ax = math.degrees(measuring_tilt_in_3D(x, y, z))
+    Ay = math.degrees(measuring_tilt_in_3D(y, x, z))
+    print("x = ", Ax)
+    print("y = ", Ay)
+
+    if Ax < -10:
+        if Ay < -10:
+            microbit.display.show(microbit.Image.ARROW_NW)
+        elif Ay > 10:
+            microbit.display.show(microbit.Image.ARROW_SW)
+        else:
+            microbit.display.show(microbit.Image.ARROW_W)
+    elif Ax > 10:
+        if Ay < -10:
+            microbit.display.show(microbit.Image.ARROW_NE)
+        elif Ay > 10:
+            microbit.display.show(microbit.Image.ARROW_SE)
+        else:
+            microbit.display.show(microbit.Image.ARROW_E)
+    else:
+        if Ay < -10 and (Ax < 10 and Ax > -10):
+            microbit.display.show(microbit.Image.ARROW_N)
+        elif Ay > 10 and (Ax < 10 and Ax > -10):
+            microbit.display.show(microbit.Image.ARROW_S)
+        else:
+            microbit.display.show(BOAT)
